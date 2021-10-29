77e41ece1df12750ac745ca104590a83527a042b
Ruben Schlaen <gschlaen@agro.uba.ar>
2021-10-29 13:24:03 -0300

    Swiper, popular, search

---
+++ lib/widgets/movie_slider.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/models/models.dart';
+
+class MovieSlider extends StatefulWidget {
+  final List<Movie> movies;
+  final String? title;
+  final Function onNextPage;
+
+  const MovieSlider({
+    Key? key,
+    required this.movies,
+    this.title,
+    required this.onNextPage,
+  }) : super(key: key);
+
+  @override
+  _MovieSliderState createState() => _MovieSliderState();
+}
+
+class _MovieSliderState extends State<MovieSlider> {
+  final ScrollController scrollController = new ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+        widget.onNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: 263,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Solo se muetra el padding si hay title. Con el ! saco el error que aparece , ya que ya verifique que venga el argumento con el if
+            if (this.widget.title != null)
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(this.widget.title!, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            SizedBox(height: 5),
+            Expanded(
+              child: ListView.builder(
+                controller: scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: this.widget.movies.length,
+                itemBuilder: (_, int index) =>
+                    _MoviePoster(this.widget.movies[index], '${widget.title}-$index-${widget.movies[index].id}'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  final Movie movie;
+  final String heroId;
+
+  const _MoviePoster(this.movie, this.heroId);
+
+  @override
+  Widget build(BuildContext context) {
+    movie.heroId = heroId;
+
+    return Container(
+      width: 130,
+      height: 190,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: movie),
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage(this.movie.fullPosterImg),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            this.movie.title,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
